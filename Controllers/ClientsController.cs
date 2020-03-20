749bebaa54daf423f5532fd863da2fceb25eead9
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 10:27:38 -0700

    update clients controller and finish with all actions

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -36,6 +36,40 @@ namespace HairSalon.Controllers
             return RedirectToAction("Index");
         }
 
-        
+        public ActionResult Details(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            return View(thisClient);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            _db.Clients.Remove(thisClient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
