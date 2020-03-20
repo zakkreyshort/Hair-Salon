0a9d701cb1450a0ae7968704f87f721d6d6f8e32
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 16:32:18 -0700

    refactor context

--- Models/HairSalonContext.cs
+++ Models/HairSalonContext.cs
@@ -7,7 +7,6 @@ namespace HairSalon.Models
     public virtual DbSet<Client> Clients { get; set; }
     public DbSet<Stylist> Stylists { get; set; }
 
-
     public HairSalonContext(DbContextOptions options) : base(options) { }
     }
 }
