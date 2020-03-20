1a412c390d51c782b61382a8164d3dd50b8e342b
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 10:11:58 -0700

    create client controller

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controllers
+    {
+        private readonly HairSalonContext _db;
+
+        public ClientsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Clients.Add(client);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
