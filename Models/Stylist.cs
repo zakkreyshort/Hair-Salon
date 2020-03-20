91e993b9fcedd6562abfe0d3ed6d66257ad34260
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 09:07:02 -0700

    create client and stylist

--- Models/Stylist.cs
+++ Models/Stylist.cs
@@ -9,6 +9,8 @@ namespace HairSalon.Models
             this.Clients = new HashSet<Client>();
         }
 
-        
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Client> Clients { get; set; }
     }
 }
