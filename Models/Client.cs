4bf0776de4caba5d8af2c969087f807c2ed691ea
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 16:08:10 -0700

    create additional views and a way to add details to page

--- Models/Client.cs
+++ Models/Client.cs
@@ -6,6 +6,8 @@ namespace HairSalon.Models
     {
         public int ClientId { get; set; }
         public string Name { get; set; }
+        public string City { get; set; }
+        public string PreferredStylist { get; set; }
         public int StylistId { get; set; }
         public virtual Stylist Stylist { get; set; }
     }
