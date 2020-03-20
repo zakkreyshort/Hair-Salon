81f70da07ac8398fef29928022ee020ebb747b18
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-20 13:32:43 -0700

    create way to add stylist details

--- Models/Stylist.cs
+++ Models/Stylist.cs
@@ -11,6 +11,7 @@ namespace HairSalon.Models
 
         public int StylistId { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
         public virtual ICollection<Client> Clients { get; set; }
     }
 }
